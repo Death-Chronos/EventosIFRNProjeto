daf49bafa52140169eb7bb6cb116e446e3c1c01c
João Victor Costa <80175567+Joao-Victor-Cunha@users.noreply.github.com>
2021-09-30 19:51:17 -0300

    Atividade de PI 202
    Modificações por atividades de Pi

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -3,12 +3,20 @@ package ifrn.pi.eventos.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ifrn.pi.eventos.models.Evento;
+
 @Controller
 public class EventosController {
 	
 	@RequestMapping("/eventos/form" )
 	public String form() {
 		return "formEventos";
+	}
+	@RequestMapping("/eventos/form/confirm")
+	public String confirmacao(Evento evento) {
+		
+		System.out.println(evento.getNome()+" "+evento.getLocal()+""+evento.getData()+" "+evento.getHorario());
+		return "confirm";
 	}
 	
 
